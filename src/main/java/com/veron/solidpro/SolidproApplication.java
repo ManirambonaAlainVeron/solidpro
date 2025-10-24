e3b8bc10a5316e714a957252e10ae8d5fcecd129
Alain Véron MANIRAMBONA <alainveron.manirambona@obr.gov.bi>
2025-10-24 11:03:48 +0200

    initializ project

---
+++ src/main/java/com/veron/solidpro/SolidproApplication.java
@@ -0,0 +1,13 @@
+package com.veron.solidpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidproApplication.class, args);
+	}
+
+}
